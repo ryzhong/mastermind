fcd663d66d6603b951bed9f0212d9ec854869d6a
ryzhong <ryzhong92@gmail.com>
2021-05-04 15:28:39 -0700

    Updated hasCorrectNumDigit to follow set rules

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -18,7 +18,7 @@ class Game extends React.Component {
     this.handlePINSubmit = this.handlePINSubmit.bind(this);
     this.isPINValid = this.isPINValid.bind(this);
     this.isPINCorrect = this.isPINCorrect.bind(this);
-    this.hasValidDigit = this.hasValidDigit.bind(this);
+    this.hasCorrectNumDigit = this.hasCorrectNumDigit.bind(this);
   }
 
   componentDidMount() {
@@ -45,7 +45,7 @@ class Game extends React.Component {
         console.log('correct')
       } else {
         console.log('wrong')
-        let guessResult = this.hasValidDigit();
+        let guessResult = this.hasCorrectNumDigit();
         if(guessResult) {
           console.log(guessResult)
         }
@@ -62,32 +62,22 @@ class Game extends React.Component {
     return pin.getPIN() === this.state.guess;
   }
 
-  hasValidDigit() {
-    let guessResults = {
-      placement: 0,
-      digits: 0,
-    }
+  hasCorrectNumDigit() {
     let arrPIN = pin.getPIN().split('');
     let arrGuess = this.state.guess.split('');
     for(let i = 0; i < this.state.pinLength; i++) {
       if(arrPIN[i] === arrGuess[i]) {
-        guessResults.placement++;
-        arrPIN[i] = null;
-        arrGuess[i] = 'found'
-        console.log(arrPIN, arrGuess)
+        return 'placement'
       }
     }
     for(let i = 0; i < this.state.pinLength; i++) {
       for(let j = 0; j < this.state.pinLength; j++) {
         if(arrGuess[i] === arrPIN[j]) {
-          guessResults.digits++;
-          arrPIN[j] = null;
-          console.log(arrPIN, j)
-          break;
+          return 'number'
         }
       }
     }
-    return guessResults;
+    return false;
   }
 
   render() {
